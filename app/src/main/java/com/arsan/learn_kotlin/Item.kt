e2b75bc3d32eb05f795792f91394ea3e8759c82f
Arsan Irianto <arsan.irianto@gmail.com>
2018-04-05 00:55:02 +0800

    Done Project 3 Anko Common and Anko Layout

--- app/src/main/java/com/arsan/learn_kotlin/Item.kt
+++
@@ -1,3 +0,0 @@
-package com.arsan.learn_kotlin
-
-data class Item(val clubName: String?, val imageName: Int?)
--- app/src/main/java/com/arsan/learn_kotlin/MainActivity.kt
+++ app/src/main/java/com/arsan/learn_kotlin/MainActivity.kt
@@ -1,37 +1,96 @@
 package com.arsan.learn_kotlin
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import com.arsan.learn_kotlin.R.array.club_image
-import com.arsan.learn_kotlin.R.array.club_name
-import kotlinx.android.synthetic.main.activity_main.*
-
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.EditText
+import com.arsan.learn_kotlin.R.color.colorAccent
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
-    private var items: MutableList<Item> = mutableListOf()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        initData()
-
-        club_list.layoutManager = LinearLayoutManager(this)
-        club_list.adapter = RecyclerViewAdapter(this, items)
+        MainActivityUI().setContentView(this)
     }
 
-    private fun initData() {
-        val name = resources.getStringArray(club_name)
-        val image = resources.obtainTypedArray(club_image)
+    class MainActivityUI : AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+            verticalLayout {
+                padding = dip(16)
+                val name = editText {
+                    hint = "Who is your name?"
 
-        items.clear()
+                }
 
-        for (i in name.indices) {
-            items.add(Item(name[i], image.getResourceId(i, 0)))
+                button("Say Hello"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                    onClick { toast("Hello, ${name.text}!") }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Alert"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        alert("Happy Coding!", "Hello ${name.text}!"){
+                            yesButton { toast("Oh...") }
+                            noButton {}
+                        }.show()
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Selector"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        val club = listOf("Barcelona", "Real Madrid", "Bayern Munchen", "Liverpool")
+                        selector("Hello, ${name.text}! What's football club do you love?", club, { dialogInterface, i ->
+                            toast("So you're love ${club[i]}, right?")
+                        })
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Snackbar"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        snackbar(name, "Hello, ${name.text}!")
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Progress Bar"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        indeterminateProgressDialog("Hello, ${name.text}! Please wait...").show()
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Go to Second Activity"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        startActivity<SecondActivity>("name" to "${name.text}")
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+            }
         }
-
-        image.recycle()
     }
 }
